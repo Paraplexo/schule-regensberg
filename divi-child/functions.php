5cfa9e6b5a45b17553fc4d9c5c702de0e21ccb23
Paraplexo <martin.vogt@avbs.ch>
2017-10-04 21:30:10 +0200

    Cosmetic changes

--- divi-child/functions.php
+++ divi-child/functions.php
@@ -1,9 +1,7 @@
 <?php
 // Load child theme instead of @import in style.css because it's performing better //
 function my_theme_enqueue_styles() {
-
     $parent_style = 'parent-style';
-
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
@@ -14,11 +12,10 @@ function my_theme_enqueue_styles() {
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 // Load Google font poppins //
-function wpb_add_google_fonts() {
- 
-wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Poppins:300,400', false ); 
+function wpb_add_google_fonts() { 
+	wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Poppins:300,400', false ); 
 }
- 
 add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
+
 
 ?>
